d67f8fae8630d742dcf175dc2027aa1b037f5d2a
shitanshusatyam <shitanshusatyam99@gmail.com>
2025-10-15 22:31:35 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,19 @@
+package logic.java;
+
+public class Marks {
+	
+	public static void main(String[]args) {
+		
+		int maths=80;
+		int english=70;
+		int java=90;
+		
+		float totalMarks=(maths+english+java);
+		float average =totalMarks/3;
+		
+		System.out.println("The Total Marks Is:"+totalMarks);
+		System.out.println("The Average Is:"+average);
+		
+	}
+
+}
